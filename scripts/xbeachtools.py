da977b528969364dfb8ec5683ae9270ef4b39df1
marlies van der lugt <marlies.vanderlugt@deltares.nl>
2022-06-23 17:02:43 +0200

    added functions to expand and/or refine a 2D XBeach grid

--- scripts/xbeachtools.py
+++ scripts/xbeachtools.py
@@ -151,3 +151,155 @@ def ygrid(y,
     #else:
     #    if area_type == 'center':
     return ygr
+
+def grid_expand_ydir(xgr,ygr,zgr,nyx,side = 'both'):
+    '''
+    grid_expand_ydir(xgr,ygr,zgr,nyx,side = 'both')
+    adds extra grid cells (nyx ones) in the y-direction based on the y spacing of the
+    last or first ycells. 
+    side = 'both' or 'left' (y=0) or 'right' (y=-1)
+    returns expanded grid where the original grid has kept its coordinates
+    
+    Author: Marlies van der Lugt
+    Revision 0 
+    
+    '''
+    
+    #rotation of the grid
+    alpha = np.arctan2(ygr[0,-1]-ygr[0,0],xgr[0,-1]-xgr[0,0])
+    
+    #the origin of the grid
+    x0 = xgr[0,0]
+    y0 = ygr[0,0]
+    
+    #to local coordinates
+    xl,yl = rotate_grid(xgr-x0,ygr-y0,alpha) 
+    if (side=='both') | (side=='left'):
+        #left side 
+        dy = yl[1,0]-yl[0,0]
+        yex = np.arange(nyx)*dy
+        yex = yex - np.max(yex)-dy
+        xex = xl[0,:]
+        Xex0,Yex0 = np.meshgrid(xex,yex)
+        xl = np.vstack([Xex0, xl])
+        yl = np.vstack([Yex0, yl])
+        zgr = np.vstack([np.tile(zgr[0,:],(nyx,1)), zgr])
+    if (side=='both') | (side=='right'):        
+        #end side 
+        dy = yl[-1,0]-yl[-2,0]
+        yex = np.arange(nyx)*dy
+        yex = yex + np.max(yl) + dy
+        xex = xl[-1,:]
+        Xex1,Yex1 = np.meshgrid(xex,yex)
+        xl = np.vstack([xl, Xex1])
+        yl = np.vstack([yl, Yex1])
+        zgr = np.vstack([zgr, np.tile(zgr[-1,:],(nyx,1))])
+    #back to world coordinates
+    xnew,ynew = rotate_grid(xl,yl,-alpha)
+    
+    #add origin again
+    xgr2 = xnew + x0
+    ygr2 = ynew + y0
+    
+    return xgr2, ygr2,zgr
+
+def grid_expand_xdir(xgr,ygr,zgr,nx):
+    '''
+    grid_expand_xdir(xgr,ygr,zgr,nx)
+    adds extra grid cells (nx ones) in the offshore x-direction based on the x spacing of the
+    last or first xcells. 
+    returns expanded grid where the original grid has kept its coordinates
+
+    Author: Marlies van der Lugt
+    Revision 0     
+    '''
+    
+    #rotation of the grid
+    alpha = np.arctan2(ygr[0,-1]-ygr[0,0],xgr[0,-1]-xgr[0,0])
+    
+    #the origin of the grid
+    x0 = xgr[0,0]
+    y0 = ygr[0,0]
+    
+    #to local coordinates
+    xl,yl = rotate_grid(xgr-x0,ygr-y0,alpha) 
+
+    #left side 
+    dx = xl[0,1]-xl[0,0]
+    xex = np.arange(nx)*dx
+    xex = xex - np.max(xex)-dx
+    yex = yl[:,0]
+    Xex0,Yex0 = np.meshgrid(xex,yex)
+    
+    xl = np.hstack([Xex0, xl])
+    yl = np.hstack([Yex0, yl])
+    zgr = np.hstack([np.tile(zgr[:,0],(nx,1)).T, zgr])
+ 
+    #back to world coordinates
+    xnew,ynew = rotate_grid(xl,yl,-alpha)
+    
+    #add origin again
+    xgr2 = xnew + x0
+    ygr2 = ynew + y0
+    
+    return xgr2, ygr2,zgr
+
+def rotate_grid(xgr,ygr,theta):
+    '''
+    rotate_grid(xgr,ygr,theta)
+    rotates a grid xgr,ygr over the angle theta (in radians)
+    
+    Author: Marlies van der Lugt
+    Revision 0     
+    '''
+    ny,nx = xgr.shape
+    coords = np.vstack((xgr.reshape(-1),ygr.reshape(-1)))
+    rotMatrix = np.array([[np.cos(theta), np.sin(theta)],[-np.sin(theta),np.cos(theta)]])
+    uv,vd = rotMatrix @ coords
+    uv = uv.reshape([ny,nx])
+    vd = vd.reshape([ny,nx])
+    return uv, vd
+
+def grid_refine_grid(xgr,ygr,xfactor = 2, yfactor = 1):
+    '''
+    grid_refine_grid(xgr,ygr,zgr,xfactor = 2, yfactor = 1, ne_layer=None)
+    refines the grid with the factor xfactor in xdirection and yfactor in y direction
+    works only on rectilinear grids
+    returns refined grid where the grid has kept its coordinates
+    
+    Author: Marlies van der Lugt
+    Revision 0 
+    '''    
+    #rotation of the grid
+    alpha = np.arctan2(ygr[0,-1]-ygr[0,0],xgr[0,-1]-xgr[0,0])
+    
+    #the origin of the grid
+    x0 = xgr[0,0]
+    y0 = ygr[0,0]
+    
+    #to local coordinates
+    xl,yl = rotate_grid(xgr-x0,ygr-y0,alpha) 
+
+    #refine the xgrid with the factor
+    xx = xl[0,:]    
+    dx = np.diff(xx)
+ 
+    xr = (xx[:-1,None] + np.linspace(0,dx,xfactor,endpoint=False).T).ravel() # xx is input array
+    xr = np.append(xr,xx[-1])
+    
+    #refine the ygrid with the factor    
+    yy = yl[:,0]
+    dy = np.diff(yy)
+    yr = (yy[:-1,None] + np.linspace(0,dy,yfactor,endpoint=False).T).ravel() # yy is input array
+    yr = np.append(yr,yy[-1])
+    
+    xr,yr = np.meshgrid(xr,yr)
+    
+    #back to world coordinates
+    xnew,ynew = rotate_grid(xr,yr,-alpha)
+    
+    #add origin again
+    xgr2 = xnew + x0
+    ygr2 = ynew + y0
+    
+    return xgr2, ygr2
