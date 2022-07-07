9b927e18469c925eb48b2da8defa8020ce25c0ad
MarliesA <marlies.vanderlugt2017@gmail.com>
2022-07-07 13:58:40 +0200

    added functions to make figures of map output

--- examples/analyse_xb_2D.py
+++ examples/analyse_xb_2D.py
@@ -1,9 +1,16 @@
 from scripts.xb_analyse import XBeachModelAnalysis
 
-r03 = XBeachModelAnalysis('r03', r'p:\11208248-xb-verkenning-hrd\modeling\r18')
+r03 = XBeachModelAnalysis('r03', r'c:\Users\marliesvanderl\phd\modeling\rvw_phzd\runs\r01')
 
 r03.load_model_setup()
 
 r03.read_modeloutput('H')
+ny, nx = r03.var['globalx'].shape
 
 fig, ax = r03.fig_check_tide_bc()
+
+r03.set_plot_localcoords(True)
+r03.set_aoi([20,445,20,220])
+
+fig, ax = r03.fig_map_diffvar('zb', '$\Delta z_b$ [m]', itend=20, it0=0)
+fig, ax = r03.fig_map_var('H','Hm0 [m]')
