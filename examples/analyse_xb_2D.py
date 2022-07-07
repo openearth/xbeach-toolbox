c2f694c3606bd8a907a0da30edca346df66450af
MarliesA <marlies.vanderlugt2017@gmail.com>
2022-07-07 14:10:48 +0200

    example usage of xb_analyse

--- examples/analyse_xb_2D.py
+++ examples/analyse_xb_2D.py
@@ -2,15 +2,22 @@ from scripts.xb_analyse import XBeachModelAnalysis
 
 r03 = XBeachModelAnalysis('r03', r'c:\Users\marliesvanderl\phd\modeling\rvw_phzd\runs\r01')
 
+# load the xbeach model set-up
 r03.load_model_setup()
 
+# get an idea of the grid shape
 r03.read_modeloutput('H')
 ny, nx = r03.var['globalx'].shape
 
+# check the boundary conditions for the tide
 fig, ax = r03.fig_check_tide_bc()
 
+# change coordinates of plots to local coordinates:
 r03.set_plot_localcoords(True)
+
+# only plot a certain Area Of Interest of the complete grid
 r03.set_aoi([20,445,20,220])
 
+#example usage map plotting
 fig, ax = r03.fig_map_diffvar('zb', '$\Delta z_b$ [m]', itend=20, it0=0)
 fig, ax = r03.fig_map_var('H','Hm0 [m]')
