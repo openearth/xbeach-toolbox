0f4bc21515fa10ea5f3ea70be08086e0ed96b3c3
menno94 <menno-deridder@hotmail.com>
2023-08-18 09:09:13 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='xbTools',
+      version='0.0.1',
+      description='Toolbox to analyse and setup xbeach models',
+      url='https://github.com/openearth/xbeach-toolbox',
+      author='Menno de ridder',
+      author_email='menno.deridder@deltares.nl',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      license='MIT',
+      packages=['xbTools'],
+      python_requires='>=3, <4',
+      install_requires=['numpy','matplotlib','netCDF4','scipy','datetime','geopandas','shapely','fiona'],
+      zip_safe=False)
