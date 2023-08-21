adc25a46a4be6820ca80e1af7aa28845148654e1
Cas van Bemmelen <38755055+casvbem@users.noreply.github.com>
2023-08-21 09:49:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='xbTools',
-      version='0.0.4',
+      version='0.0.5',
       description='Toolbox to analyse and setup xbeach models',
       url='https://github.com/openearth/xbeach-toolbox',
       # Authors: Menno de Ridder (menno.deridder@deltares.nl), Cas van Bemmelen (cas.van.bemmelen@witteveenbos.com)
